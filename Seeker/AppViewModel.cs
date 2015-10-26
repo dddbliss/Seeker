206665439bbe1f228c73c75a470f3a992e111aab
Ryan Currie <dddbliss@gmail.com>
2015-10-25 17:41:57 -0700

    Initial Application

---
+++ Seeker/AppViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.ComponentModel.Composition;
+
+namespace Seeker
+{
+    [Export(typeof(AppViewModel))]
+    public class AppViewModel : PropertyChangedBase
+    {
+        private IWindowManager _windowManager;
+
+        [ImportingConstructor]
+        public AppViewModel(IWindowManager windowManager)
+        {
+            _windowManager = windowManager;
+        }
+    }
+}
